05b315df804ea8da5a8a79624b165f56c44c700b
Ruth Poirier <125494680+PeartreeAndWolf@users.noreply.github.com>
2023-03-02 15:41:10 -0600

    finished studio + 2 bonus items

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 // TODO: add code here
-test
-test tessssst
-testing 12345678910
+window.addEventListener("load", function(){
+
+    const container = document.getElementById("container");
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+
+            data.sort(function(a, b) {
+                return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+            });
+
+            for(let i=0; i < data.length; i++){
+                let astronaut = data[i];
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li style="${astronaut.active ? "color:green" : ""}">Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                <img class="avatar" src="${astronaut.picture}">
+            </div>
+        `
+
+        }
+         });
+    });
+});
